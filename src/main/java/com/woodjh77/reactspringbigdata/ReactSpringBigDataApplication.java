adfd07bc0f8001f0969edd51bc4205b7b1ae803b
John Wood <woodjh@apsu.edu>
2018-09-16 15:25:12 -0500

    testing 2

--- src/main/java/com/woodjh77/reactspringbigdata/ReactSpringBigDataApplication.java
+++ src/main/java/com/woodjh77/reactspringbigdata/ReactSpringBigDataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReactSpringBigDataApplication {
 
 	public static void main(String[] args) {
+		//Hi.
 		SpringApplication.run(ReactSpringBigDataApplication.class, args);
 	}
 }
